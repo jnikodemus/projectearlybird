9de0642769e069625c4aae50b5b7241bfe93172c
JNikodemus <julian.nikodemus@th-bingen.de>
2019-09-04 14:33:49 +0200

    Added some Code an Activites

--- app/src/main/java/de/ntbit/projectearlybird/connection/ParseConnection.kt
+++ app/src/main/java/de/ntbit/projectearlybird/connection/ParseConnection.kt
@@ -1,10 +1,27 @@
 package de.ntbit.projectearlybird.connection
 
+import android.content.Context
+import android.util.Log
 import de.ntbit.projectearlybird.manager.ParseManager
+import com.parse.Parse
+import java.util.logging.Logger
+
 
 class ParseConnection {
 
-    fun getParseManager(x: Any, y: Any): Unit {
+    companion object {
+        fun initialize(context: Context) : Unit {
+            Parse.initialize(
+                Parse.Configuration.Builder(context)
+                    .applicationId("pYIuK6xeAMNkL2IYpOEWIiAoacyr8jEyTja8LqxV")
+                    .clientKey("mpF0Gq4uUR9e7qSgFBefH6UPgJxNdaQyHxEg73tH")
+                    .server("https://parseapi.back4app.com")
+                    .build()
+            )
+        }
 
+        fun getParseManager(x: Any, y: Any): Unit {
+
+        }
     }
 }
--- app/src/main/java/de/ntbit/projectearlybird/ui/LoginActivity.kt
+++ app/src/main/java/de/ntbit/projectearlybird/ui/LoginActivity.kt
@@ -4,10 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import de.ntbit.projectearlybird.R
 
+import de.ntbit.projectearlybird.connection.ParseConnection
+import java.util.logging.Logger
+
+
 class LoginActivity : AppCompatActivity() {
+
+    private val log = Logger.getLogger(this.localClassName)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        ParseConnection.initialize(this)
     }
 }
