a45693c828988cb7ab6337c3c1cbc7ef9d05ce2d
JNikodemus <julian.nikodemus@th-bingen.de>
2019-09-04 14:44:32 +0200

    Added ActivityRegister

--- app/src/main/java/de/ntbit/projectearlybird/ui/LoginActivity.kt
+++ app/src/main/java/de/ntbit/projectearlybird/ui/LoginActivity.kt
@@ -5,12 +5,9 @@ import android.os.Bundle
 import de.ntbit.projectearlybird.R
 
 import de.ntbit.projectearlybird.connection.ParseConnection
-import java.util.logging.Logger
 
 
 class LoginActivity : AppCompatActivity() {
-
-    private val log = Logger.getLogger(this.localClassName)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
