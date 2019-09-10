8c5bec0d339edbb1dbd326d126b32a0b70495961
JNikodemus <julian.nikodemus@th-bingen.de>
2019-09-10 21:48:39 +0200

    Removed Actionbar and added custom toolbar

--- app/src/main/java/de/ntbit/projectearlybird/ui/HomeActivity.kt
+++ app/src/main/java/de/ntbit/projectearlybird/ui/HomeActivity.kt
@@ -1,16 +1,39 @@
 package de.ntbit.projectearlybird.ui
 
 import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.ui.AppBarConfiguration
+
 import de.ntbit.projectearlybird.R
 import java.util.logging.Logger
 
 class HomeActivity : AppCompatActivity() {
 
     private val log = Logger.getLogger(this::class.java.simpleName)
+    //private val navController : NavController = NavController(this)
+    //private val appBarConfiguration = AppBarConfiguration(navController.graph)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        init()
+    }
+
+    private fun init() {
+        /*
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if(destination.id == R.id.full_screen_destination) {
+                toolbar.visibility = View.GONE
+                bottomNavigationView.visibility = View.GONE
+            } else {
+                toolbar.visibility = View.VISIBLE
+                bottomNavigationView.visibility = View.VISIBLE
+            }
+        }
+        */
+
     }
 }
