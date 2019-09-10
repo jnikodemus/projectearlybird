61f65ca3088934ec424020ec435d5b0e78072f4b
JNikodemus <julian.nikodemus@th-bingen.de>
2019-09-10 22:39:26 +0200

    added clicklistener to toolbar

--- app/src/main/java/de/ntbit/projectearlybird/ui/HomeActivity.kt
+++ app/src/main/java/de/ntbit/projectearlybird/ui/HomeActivity.kt
@@ -1,28 +1,34 @@
 package de.ntbit.projectearlybird.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.ui.AppBarConfiguration
 
 import de.ntbit.projectearlybird.R
+import kotlinx.android.synthetic.main.toolbar.*
 import java.util.logging.Logger
 
 class HomeActivity : AppCompatActivity() {
 
     private val log = Logger.getLogger(this::class.java.simpleName)
-    //private val navController : NavController = NavController(this)
+    private val navController : NavController = NavController(this)
     //private val appBarConfiguration = AppBarConfiguration(navController.graph)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        init()
+        initialize()
     }
 
-    private fun init() {
+    private fun initialize() {
+        toolbar.setOnClickListener {
+            Toast.makeText(applicationContext, "Toolbar clicked", Toast.LENGTH_SHORT).show()
+        }
         /*
         navController.addOnDestinationChangedListener { _, destination, _ ->
             if(destination.id == R.id.full_screen_destination) {
