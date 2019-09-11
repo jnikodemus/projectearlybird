2e7f5a9ef3b726bbcfcacaf2605cd485522671ed
JNikodemus <julian.nikodemus@th-bingen.de>
2019-09-11 23:25:29 +0200

    Added Settings to Menu

--- app/src/main/java/de/ntbit/projectearlybird/ui/HomeActivity.kt
+++ app/src/main/java/de/ntbit/projectearlybird/ui/HomeActivity.kt
@@ -3,8 +3,8 @@ package de.ntbit.projectearlybird.ui
 import androidx.appcompat.app.AppCompatActivity
 
 import android.os.Bundle
+import androidx.annotation.ContentView
 import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 
@@ -16,9 +16,8 @@ import java.util.logging.Logger
 class HomeActivity : AppCompatActivity() {
 
     private val log = Logger.getLogger(this::class.java.simpleName)
-    private var drawer: DrawerLayout? = null
-    //private val navController : NavController = NavController(this)
-    //private val appBarConfiguration = AppBarConfiguration(navController.graph)
+    private lateinit var drawer: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,33 +26,24 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private fun initialize() {
-        val toolbar: Toolbar = toolbar
+        /* Set toolbar */
+        val toolbar = toolbar
         setSupportActionBar(toolbar)
+
+        /* Set Navigation Drawer */
         drawer = drawer_layout
-        log.info("drawer is null? " + (drawer == null))
-        val toggle = ActionBarDrawerToggle(
+        toggle = ActionBarDrawerToggle(
             this, drawer, toolbar,
             R.string.menu_drawer_open,
-            R.string.menu_drawer_close)
-        drawer?.addDrawerListener(toggle)
+            R.string.menu_drawer_close
+        )
+        drawer.addDrawerListener(toggle)
         toggle.syncState()
-
-        /*
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            if(destination.id == R.id.full_screen_destination) {
-                toolbar.visibility = View.GONE
-                bottomNavigationView.visibility = View.GONE
-            } else {
-                toolbar.visibility = View.VISIBLE
-                bottomNavigationView.visibility = View.VISIBLE
-            }
-        }
-        */
     }
 
     override fun onBackPressed() {
-        if(drawer?.isDrawerOpen(GravityCompat.START)!!)
-            drawer?.closeDrawer(GravityCompat.START)
+        if (drawer.isDrawerOpen(GravityCompat.START))
+            drawer.closeDrawer(GravityCompat.START)
         else super.onBackPressed()
     }
 }
---
+++ app/src/main/java/de/ntbit/projectearlybird/ui/SettingActivity.kt
@@ -0,0 +1,13 @@
+package de.ntbit.projectearlybird.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import de.ntbit.projectearlybird.R
+
+class SettingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
