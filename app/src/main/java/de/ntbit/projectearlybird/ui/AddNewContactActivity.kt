ea0913ba99e75b41d3b9b34882fb09e033fe8062
ptxd <pascal.truempert@gmx.de>
2020-03-22 02:00:24 +0100

    Working on "searching and adding specific User"

---
+++ app/src/main/java/de/ntbit/projectearlybird/ui/AddNewContactActivity.kt
@@ -0,0 +1,36 @@
+package de.ntbit.projectearlybird.ui
+
+import android.app.SearchManager
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.parse.ParseQuery
+import com.parse.ParseUser
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import de.ntbit.projectearlybird.R
+import de.ntbit.projectearlybird.adapter.UserItem
+import de.ntbit.projectearlybird.model.Message
+import kotlinx.android.synthetic.main.activity_add_new_contact.*
+
+
+class AddNewContactActivity : AppCompatActivity() {
+
+    private val adapter = GroupAdapter<GroupieViewHolder>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_new_contact)
+
+        if(Intent.ACTION_SEARCH == intent.action) {
+            intent.getStringExtra(SearchManager.QUERY)?.also { username ->
+                val query = ParseUser.getQuery()
+                query.whereContains("username", username)
+                adapter.add(UserItem(query.first))
+                adapter.notifyDataSetChanged()
+            }
+        }
+        add_new_contact_rv.adapter = adapter
+    }
+}
